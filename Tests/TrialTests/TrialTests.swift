011435a288a9317e58a4cdbe0611a1356b332535
Anas <mahdisomjee04@gmail.com>
2024-06-03 10:28:55 +0530

    Added SPM

---
+++ Tests/TrialTests/TrialTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Trial
+
+final class TrialTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
